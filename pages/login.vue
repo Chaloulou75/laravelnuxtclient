0e9421ef833938a8fc86a4f2df4b9f4a824b2544
Chaloulou75 <c.jeandey@gmail.com>
2021-03-02 18:04:26 -0300

    first commit nuxt client

---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <a href="#" @click.prevent="login" class="p-8 text-3xl font-semibold text-gray-700 mx-auto w-full">Login</a>
+
+    <div v-if="$auth.loggedIn" class="p-8 text-5xl font-bold text-blue-700 mx-auto w-full">{{ $auth.user.name }}</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  methods: {
+    login () {
+      this.$auth.loginWith('laravelSanctum', {
+        data: {
+          email: 'c.jeandey@gmail.com',
+          password: 'password',
+        }
+      })
+    }
+  }
+
+}
+</script>
